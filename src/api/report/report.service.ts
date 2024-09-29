4ae16481e77eef5cbe5a41ed647933fed0a7e3f7
mrszympek <piela.bartek@gmail.com>
2024-09-29 11:45:54 +0200

    fix: remove mocks

--- src/api/report/report.service.ts
+++ src/api/report/report.service.ts
@@ -1,65 +1,6 @@
 import { ReportResponse } from '@/api/report/models/report.model';
 import { httpClient } from '@/api/interceptors';
 
-const mockedData: ReportResponse = {
-  results: {
-    transcription: 9,
-    yourScore: 10,
-    video: 2,
-    audio: 3,
-  },
-  transcription: [
-    {
-      recommendations: 'Recommendation 1',
-      videoExample: 11,
-      comment: 'Comment 1',
-      score: 1,
-      area: 'Area 1',
-    },
-    {
-      recommendations: 'Recommendation 2',
-      videoExample: 22,
-      comment: 'Comment 2',
-      score: 2,
-      area: 'Area 2',
-    },
-  ],
-  video: [
-    {
-      recommendations: 'Recommendation 1',
-      videoExample: 1,
-      comment: 'Comment 1',
-      score: 1,
-      area: 'Area 1',
-    },
-    {
-      recommendations: 'Recommendation 2',
-      videoExample: 20,
-      comment: 'Comment 2',
-      score: 2,
-      area: 'Area 2',
-    },
-  ],
-  audio: [
-    {
-      recommendations: 'Recommendation 1',
-      videoExample: 4,
-      comment: 'Comment 1',
-      score: 1,
-      area: 'Area 1',
-    },
-    {
-      recommendations: 'Recommendation 2',
-      videoExample: 22,
-      comment: 'Comment 2',
-      score: 2,
-      area: 'Area 2',
-    },
-  ],
-  videoUrl: 'https://www.youtube.com/watch?v=6n3pFFPSlW4',
-  processing: false,
-};
-
 export async function getReport({
   session,
   user_id,
@@ -76,25 +17,6 @@ interface AllReportsResponse {
   label: string;
   id: number;
 }
-
-const mockedReportsAll = [
-  {
-    label: 'Raport 1',
-    id: 1,
-  },
-  {
-    label: 'Raport 2',
-    id: 2,
-  },
-  {
-    label: 'Raport 3',
-    id: 3,
-  },
-  {
-    label: 'Raport 4',
-    id: 4,
-  },
-];
 
 export async function getAllReports({
   userId,
